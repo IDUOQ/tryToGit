9a14813d964a8e716b5a94300b40c78b3b6371e6
Vlad <riavkin.vm@phystech.edu>
2023-03-01 23:42:08 +0500

    fff

---
+++ cmake-build-debug/CMakeFiles/trainingForGithub.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trainingForGithub.dir/main.cpp.o"
+  "CMakeFiles/trainingForGithub.dir/main.cpp.o.d"
+  "trainingForGithub"
+  "trainingForGithub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trainingForGithub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/trainingForGithub.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for trainingForGithub.
+# This may be replaced when dependencies are built.
