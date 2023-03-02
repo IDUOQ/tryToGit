33c2e3e9ada300b15f547f7489549aef40e323c4
Vlad <riavkin.vm@phystech.edu>
2023-03-02 16:00:15 +0500

    second

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    const int c1 = 42;  // эта константа известна в compile time
+    int x;
+    std::cin >> x;
+    const int c2 = 2 * x;  // значение становится известным только в runtime
+    int a[c2];
 }
